2bf0b7e86764eafea1725f264fbbe4b353f25e72
sa2321 <118229043+sa2321@users.noreply.github.com>
2023-11-06 22:02:59 +0000

    Update solvers.py

--- nonlinear_solvers/solvers.py
+++ nonlinear_solvers/solvers.py
@@ -8,61 +8,32 @@ class ConvergenceError(Exception):
 
 
 def newton_raphson(f, df, x_0, eps=1.0e-5, max_its=20):
-    """Solve a nonlinear equation using Newton-Raphson iteration.
-
-    Solve f==0 using Newton-Raphson iteration.
-
-    Parameters
-    ----------
-    f : function(x: float) -> float
-        The function whose root is being found.
-    df : function(x: float) -> float
-        The derivative of f.
-    x_0 : float
-        The initial value of x in the iteration.
-    eps : float
-        The solver tolerance. Convergence is achieved when abs(f(x)) < eps.
-    max_its : int
-        The maximum number of iterations to be taken before the solver is taken
-        to have failed.
-
-    Returns
-    -------
-    float
-        The approximate root computed using Newton iteration.
-    """
-    # Delete these two lines when implementing the method.
+    """"""
+    x = x_0
+     for _ in range(max_its):
+        x = x - f(x) / df(x)
+        if abs(f(x)) < eps:
+            return x
+  
+    raise ConvergenceError("Newton-Raphson iteration did not converge")
     raise NotImplementedError
-
 
 def bisection(f, x_0, x_1, eps=1.0e-5, max_its=20):
-    """Solve a nonlinear equation using bisection.
+    """"""
+    if f(x_0) * f(x_1) >= 0:
+        raise ValueError("Initial points must have opposite signs for bisection")
 
-    Solve f==0 using bisection starting with the interval [x_0, x_1]. f(x_0)
-    and f(x_1) must differ in sign.
+    for _ in range(max_its):
+        x_star = (x_0 + x_1) / 2
+        if abs(f(x_star)) < eps:
+            return x_star
+        if f(x_0) * f(x_star) > 0:
+            x_0 = x_star
+        else:
+            x_1 = x_star
 
-    Parameters
-    ----------
-    f : function(x: float) -> float
-        The function whose root is being found.
-    x_0 : float
-        The left end of the initial bisection interval.
-    x_1 : float
-        The right end of the initial bisection interval.
-    eps : float
-        The solver tolerance. Convergence is achieved when abs(f(x)) < eps.
-    max_its : int
-        The maximum number of iterations to be taken before the solver is taken
-        to have failed.
-
-    Returns
-    -------
-    float
-        The approximate root computed using bisection.
-    """
-    # Delete these two lines when implementing the method.
+    raise ConvergenceError("Bisection method did not converge")
     raise NotImplementedError
-
 
 def solve(f, df, x_0, x_1, eps=1.0e-5, max_its_n=20, max_its_b=20):
     """Solve a nonlinear equation.
