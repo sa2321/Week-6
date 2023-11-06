40b4575197f2da63e291680dceb46f8e2b0a9189
sa2321 <118229043+sa2321@users.noreply.github.com>
2023-11-06 22:18:43 +0000

    Update solvers.py

--- nonlinear_solvers/solvers.py
+++ nonlinear_solvers/solvers.py
@@ -36,35 +36,9 @@ def bisection(f, x_0, x_1, eps, max_its):
     raise NotImplementedError
 
 def solve(f, df, x_0, x_1, eps=1.0e-5, max_its_n=20, max_its_b=20):
-    """Solve a nonlinear equation.
-
-    solve f(x) == 0 using Newton-Raphson iteration, falling back to bisection
-    if the former fails.
-
-    Parameters
-    ----------
-    f : function(x: float) -> float
-        The function whose root is being found.
-    df : function(x: float) -> float
-        The derivative of f.
-    x_0 : float
-        The initial value of x in the Newton-Raphson iteration, and left end of
-        the initial bisection interval.
-    x_1 : float
-        The right end of the initial bisection interval.
-    eps : float
-        The solver tolerance. Convergence is achieved when abs(f(x)) < eps.
-    max_its_n : int
-        The maximum number of iterations to be taken before the newton-raphson
-        solver is taken to have failed.
-    max_its_b : int
-        The maximum number of iterations to be taken before the bisection
-        solver is taken to have failed.
-
-    Returns
-    -------
-    float
-        The approximate root.
-    """
-    # Delete these two lines when implementing the method.
+    """"""
+    try:
+        return newton_raphson(f, df, x_0, eps, max_its_n)
+    except ConvergenceError:
+        return bisection(f, x_0, x_1, eps, max_its_b)
     raise NotImplementedError
